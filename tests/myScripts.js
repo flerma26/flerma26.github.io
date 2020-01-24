c3139d1fd91ce2ad0d9123a54cae926f1338603b
flerma26 <55292622+flerma26@users.noreply.github.com>
2020-01-24 13:26:52 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,10 +1,19 @@
-function myLoop(){
-var start = document.getElementById("start").value;   
-var end = document.getElementById("end").value;
-console.log(start); 
-for(var i = start; i <= end; i++)
-  {
-    document.write("<p>" + i + "</p>");
-  }
+function myLoop() {
 
+var start = document.getElementById("start").value;
+var end= document.getElementById("end").value;
+var jump= document.getElementById("jump").value;
+jump = Number(jump);
+start = Number(start);
+end = Number(end);
+console.log(typeof jump);
+console.log(typeof start);  
+console.log(typeof end);
+  for (var i = start; i <= end; i+=jump) {
+    
+    
+    document.write( '<p>' + i + '</p>');
+  // document.write(jump);
+  // console.log(i);
+}
 }
